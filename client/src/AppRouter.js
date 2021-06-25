1e79d02ad6118fa9a119bd9feaac29c2569c5ecc
abran <alexandru.bran@nokia.com>
2021-06-25 15:09:11 +0300

    dinamic home page url

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -1,12 +1,13 @@
 import React from "react";
 import HomePage from "./components/Homepage.jsx";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import {config} from "./config"
 
 export const AppRouter = () => {
   return (
     <Router>
       <div>
-        <Route exact path="/nptbeta" component={HomePage} />
+        <Route exact path={config.baseLOCATION + "/nptbeta"} component={HomePage} />
       </div>
     </Router>
   );
