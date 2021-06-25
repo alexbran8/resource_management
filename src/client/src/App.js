9a0ba7b02fe0dc6d18f7434ffc17aa28aea61cd8
abran <alexandru.bran@nokia.com>
2021-06-25 09:47:10 +0300

    base project add

---
+++ src/client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { AppRouter } from "./AppRouter";
+
+class App extends Component {
+  render() {
+    return <AppRouter />;
+  }
+}
+
+
+// test
+export default App;
