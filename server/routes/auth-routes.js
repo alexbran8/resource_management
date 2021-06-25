1cb86827a444a63ee3cd9007ffb35b1c1804008e
abran <alexandru.bran@nokia.com>
2021-06-25 14:37:12 +0300

    login test

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const passport = require("passport");
-const CLIENT_HOME_PAGE_URL = "http://localhost:3000"
+// const CLIENT_HOME_PAGE_URL = "http://localhost:3000"
+const CLIENT_HOME_PAGE_URL = process.env.NODE_ENV === `development` ? "http://localhost:3000" :  'https://apps.gdceur.eecloud.dynamic.nsn-net.net/nptbeta/';
 
 // when login is successful, retrieve user info
 router.get("/login/success", (req, res) => {
