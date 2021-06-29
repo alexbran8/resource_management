f787e388495e05a42d60314e839a19681bc66d29
abran <alexandru.bran@nokia.com>
2021-06-29 10:18:48 +0300

    router review

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -1,19 +1,19 @@
 import React from "react";
-import HomePage from "./components/Homepage.jsx";
+import HomePage from "./components/HomePage.jsx";
+import Header from "./components/Header.jsx";
 import NormCheck from './components/NormCheck'
 import Calendar from './components/Calendar.js'
 import { HashRouter,Route } from "react-router-dom";
-import {config} from "./config"
 import { signUpForm } from './components/SignUp'
 
 export const AppRouter = () => {
   return (
     <HashRouter  >
-        <HomePage />
-        {/* <Route exact path={config.baseLOCATION + "/"} component={HomePage} /> */}
-        <Route path={"/normcheck/"} component={NormCheck} />
-        <Route exact path={"/signup"} component={signUpForm} />
-        <Route path={"/schedule"} component={Calendar} />
+        <Header />``
+        <Route  path={"/home/"} component={HomePage} />
+        <Route  path={"/normcheck/"} component={NormCheck} />
+        <Route  path={"/signup"} component={signUpForm} />
+        <Route  path={"/schedule"} component={Calendar} />
     </HashRouter>
   );
 };
