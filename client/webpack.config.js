b7cf2bbaec6b032b4e94bdf43c82747827e79211
abran <alexandru.bran@nokia.com>
2022-05-11 15:08:44 +0300

    bundle name update

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,10 +23,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, '/public/dist/'),
     filename: 'main.js',
-    output: {
     chunkFilename: '[name].bundle.js',
-    
-  },
     publicPath: location,
   },
   devServer: {
