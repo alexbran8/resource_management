ab75668148952c700f7367df8f8ff3e8dd7334da
abran <alexandru.bran@nokia.com>
2022-07-05 15:48:40 +0300

    updates

--- sql/abasences.sql
+++ sql/abasences.sql
@@ -1,9 +1,20 @@
 SELECT * FROM (
 (SELECT start, "end", nokiaid, title, "bgColor", type, status, replacement, "createdBy", id, task_type, task_admin, task_operational, notifications, comments, task_status, "creationDate", norm, uid
 	FROM public.events
-	where type in ('Vacation', 'Medical Leave', 'Blood donation')
+	where type in ('Vacation')
+ 	and
+ 	status = 'L3'
 	and
 	EXTRACT(YEAR FROM "end") = 2022
+ 	and
+ 	(EXTRACT(MONTH FROM "start") >= 7
+ 	OR
+ 	EXTRACT(MONTH FROM "end") >= 7)
+ 	and
+  	(EXTRACT(MONTH FROM "start") <= 8
+ 	OR
+ 	EXTRACT(MONTH FROM "end") <= 8)
+ 
 	order by "start" asc) as sql1
 	
 	LEFT JOIN
